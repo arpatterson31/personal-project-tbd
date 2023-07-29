1e425fad59386daf36adc3c222dfe776a1329044
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-07-28 23:54:46 -0700

    splash page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <h1>App</h1>
+    <div className="wrapper">
+      <h1>coming soon<span className="dot">...</span></h1>
+      <p>A totally awesome website</p>
+      <div className="icons">
+        <a href="https://github.com/arpatterson31"><i className="fa-brands fa-github-alt"></i></a>
+        <a href="https://www.linkedin.com/in/audrey-patterson31/"><i className="fa-brands fa-linkedin"></i></a>
+      </div>
+    </div>
   )
 }
 
 export default App;
+
