4812cdfbd5e2c45a7c790fc075d80d794a41ebec
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-07-29 23:18:04 -0700

    set up routes on app for different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import './App.css';
 import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Profile from './pages/Profile';
+import TermsConditions from './pages/TermsConditions';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+       <Header />
+        <Routes>
+          <Route 
+          exact 
+          path="/" 
+          element={<Home />}
+          />
+          <Route 
+          path="/project_board"
+          element={<Projects />}
+          />
+          <Route 
+          path="/profile"
+          element={<Profile />}
+          />
+          <Route 
+          path="/terms_and_conditions"
+          element={<TermsConditions />}
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
