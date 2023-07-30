66ad2443a3d4f84c87682b73e0d1ea9943c92b0f
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-07-29 22:48:29 -0700

    removes code for the splash page about the website coming soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css';
-
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="wrapper">
-      <h1>coming soon<span className="dot">...</span></h1>
-      <p>A totally awesome website</p>
-      <div className="icons">
-        <a href="https://github.com/arpatterson31"><i className="fa-brands fa-github-alt"></i></a>
-        <a href="https://www.linkedin.com/in/audrey-patterson31/"><i className="fa-brands fa-linkedin"></i></a>
-      </div>
-    </div>
+    <>
+      <Home />
+    </>
   )
 }
 
