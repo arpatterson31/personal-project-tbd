4d2067410245beb5e3c7d5608c715099fc28261a
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-07-30 23:31:08 -0700

    adding boiler plate terms and conditions for now

--- src/pages/TermsConditions.jsx
+++ src/pages/TermsConditions.jsx
@@ -1,6 +1,76 @@
 function TermsConditions() {
   return (
-    <p>Legal jargon with terms and conditions</p>
+    <p>
+    ---
+    
+    [Your Website Name]
+    
+    Terms and Conditions
+    
+    Effective Date: [Insert Effective Date]
+    
+    1. Acceptance of Terms
+    
+    Welcome to [Your Website Name]! By accessing or using our website and services, you agree to be bound by these Terms and Conditions, our Privacy Policy, and any additional guidelines, policies, or rules posted on the website. If you do not agree with any of these terms, please refrain from using the website.
+    
+    2. Description of Services
+    
+    [Your Website Name] is a social media platform that allows users to connect, make friends, and collaborate on project ideas. Users can post project ideas, share updates, and interact with others through various communication features provided by the platform.
+    
+    3. User Registration
+    
+    3.1 Eligibility: To use [Your Website Name], you must be at least 18 years old or the age of legal majority in your jurisdiction. By registering, you represent and warrant that you meet these eligibility requirements.
+    
+    3.2 Account Information: When registering, you agree to provide accurate and complete information. You are responsible for maintaining the confidentiality of your account credentials and for any activity that occurs under your account.
+    
+    4. User Content
+    
+    4.1 Content Ownership: You retain ownership of any content you post on [Your Website Name]. By posting content, you grant [Your Website Name] a non-exclusive, worldwide, royalty-free, sublicensable, and transferable license to use, reproduce, modify, adapt, publish, translate, distribute, and display such content for the purpose of operating and improving the platform.
+    
+    4.2 Responsibility for Content: You are solely responsible for the content you post on [Your Website Name]. Content must comply with applicable laws and must not infringe on the rights of third parties. [Your Website Name] reserves the right to remove or modify any content that violates these terms.
+    
+    5. User Conduct
+    
+    5.1 Prohibited Activities: When using [Your Website Name], you agree not to:
+    
+    5.1.1 Violate any applicable laws or regulations;
+    5.1.2 Use the platform for any unlawful, harmful, or malicious purposes;
+    5.1.3 Impersonate another person or entity;
+    5.1.4 Engage in any form of harassment, hate speech, or discrimination;
+    5.1.5 Introduce malware or other malicious code;
+    5.1.6 Attempt to gain unauthorized access to other users' accounts;
+    5.1.7 Engage in any activity that interferes with or disrupts the platform's functionality;
+    5.1.8 Collect or store other users' personal information without their consent.
+    
+    6. Intellectual Property
+    
+    6.1 [Your Website Name] Content: All content provided by [Your Website Name], including logos, trademarks, graphics, and text, is the property of [Your Website Name] and is protected by copyright and other intellectual property laws.
+    
+    6.2 User Contributions: Users retain ownership of their content, as stated in Section 4.1. Users are responsible for ensuring that their content does not infringe on the intellectual property rights of others.
+    
+    7. Termination
+    
+    [Your Website Name] reserves the right to terminate or suspend any user's account at its discretion, with or without cause. Upon termination, all licenses granted to [Your Website Name] regarding user content will terminate.
+    
+    8. Modification of Terms
+    
+    [Your Website Name] may update or modify these Terms and Conditions at any time. Any changes will be effective upon posting on the website. Your continued use of the platform after such changes signifies your acceptance of the modified terms.
+    
+    9. Disclaimer of Warranties
+    
+    [Your Website Name] provides the platform "as is" and without warranties of any kind, whether express or implied. [Your Website Name] does not warrant that the platform will be error-free or uninterrupted.
+    
+    10. Limitation of Liability
+    
+    To the maximum extent permitted by law, [Your Website Name] shall not be liable for any indirect, incidental, special, consequential, or punitive damages arising out of or in connection with the use or inability to use the platform.
+    
+    11. Governing Law
+    
+    These Terms and Conditions shall be governed by and construed in accordance with the laws of [Your Jurisdiction].
+    
+    ---
+    
+    Remember, this template is just a starting point. Ensure that you have a legal professional review and customize it to fit your specific needs and the laws of your jurisdiction. As your website evolves, you may need to update the Terms and Conditions to reflect any changes in functionality or legal requirements.</p>
   )
 }
 
