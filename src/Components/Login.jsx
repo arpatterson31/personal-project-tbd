28f8614328c04f6b2ac91b7f56ecb625ed079dfb
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-08-02 16:54:11 -0700

    updates login button to take in a text prop so it can be reuseable

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,9 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
-const LoginButton = () => {
+const LoginButton = ({text = 'Log In'}) => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button onClick={() => loginWithRedirect()}>{text}</button>;
 };
 
 export default LoginButton;
