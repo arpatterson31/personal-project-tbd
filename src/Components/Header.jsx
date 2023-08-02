bfda3641de2c95d65c577719dfd41a363a7658ec
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-08-01 17:21:04 -0700

    starter code for header gets a logo in there that will be replaced and the avatar shows up once the user is logged in. styling still needs work

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,105 @@
+import * as React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import LoggedInPic from './LoggedInPic';
+import Login from './Login';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const settings = ['Profile', 'Logout'];
 
-function Header() {
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: '#9BABBA'
+    }
+  }
+})
+
+const Header = () => {
+
+  const { isAuthenticated } = useAuth0();
+
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    //update this to take user to profile page or have them logout
+    setAnchorElUser(null);
+  };
+
   return (
-    <>
-    <p>This is gonna be the header</p>
-    </>
+    <ThemeProvider theme={theme}>
+      <AppBar position="sticky" color="secondary">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <ConnectWithoutContactIcon sx={{ fontSize: 40, display: { xs: 'flex', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'flex' },
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+                flexGrow: 1
+              }}
+            >
+              DevConnect
+            </Typography>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                {
+                  isAuthenticated ?
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                   <LoggedInPic /> 
+                  </IconButton>
+                  :
+                  <Login />
+                }   
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   )
 }
 
