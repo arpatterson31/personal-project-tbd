02c00e7978a00125388e831aa67e96eb5ec404ac
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-08-02 17:15:15 -0700

    adds grid to the splash page

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,11 +1,18 @@
 import Login from '../Components/Login';
+import Grid from '@mui/material/Grid';
+
 
 const Splash = () => {
   return (
-    <>
-    <h1>Splash page for now..</h1>
-    <Login text="Join the Community / Sign Up"/>
-    </>
+    <Grid 
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <h1>Splash page for now..</h1>
+      <Login text="Join the Community / Sign Up" />
+    </Grid>
   )
 }
 
